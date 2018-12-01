1eb60fb418dfa79360faef640ab4cb62c7ed38bc
Rumir125 <34058167+Rumir125@users.noreply.github.com>
2018-12-01 16:39:53 +0100

    Main Camera simple script

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject Player;
+
+    void Start()
+    {
+        if (GameObject.Find("pikachu"))
+        {
+            Player = GameObject.Find("pikachu");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+    }
+}
