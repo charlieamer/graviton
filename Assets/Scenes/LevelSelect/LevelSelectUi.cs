0f940d9f0570709dc54065507a7192e2ad2ba7cd
Amer Zavlan <amer_zavlan@live.com>
2018-12-01 13:37:20 +0100

    Main menu and level select scene

---
+++ Assets/Scenes/LevelSelect/LevelSelectUi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectUi : MonoBehaviour
+{
+    void LevelSelected(int LevelNum)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Level" + LevelNum);
+    }
+
+    public void Level1Selected()
+    {
+        LevelSelected(1);
+    }
+
+    public void Level2Selected()
+    {
+        LevelSelected(2);
+    }
+
+    public void Level3Selected()
+    {
+        LevelSelected(3);
+    }
+}
---
+++ Assets/Scenes/MainMenu/MainMenuUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OnQuitButtonClicked() {
+        Debug.Log("Quit clicked");
+        Application.Quit();
+    }
+
+    public void OnLevelSelectButtonClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("LevelSelect");
+    }
+}
