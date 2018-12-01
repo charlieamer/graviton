b7259a64897183a6554f2053ed3bbbe4abd0b7e3
Vildan Tursic <vildantursic@hotmail.com>
2018-12-01 12:39:39 +0100

    gravity created, ecs removed

---
+++ Assets/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gravity : MonoBehaviour
+{
+
+    private Rigidbody2D character;
+
+    private void Start()
+    {
+        character = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        character.AddForce(Vector2.left * 9.8f);
+    }
+}
