df941003b53b94fc4c0d287181a8a2f954368be0
Rumir125 <34058167+Rumir125@users.noreply.github.com>
2018-12-06 19:41:10 +0100

    Ragdoll Hero Done

---
+++ Assets/UpdateAnimationValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAnimationValues : MonoBehaviour
+{
+    public Animator animator;
+    private float LastXPosition;
+    private float CurrentXPosition;
+    private float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LastXPosition = transform.position.x;
+        CurrentXPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LastXPosition = CurrentXPosition;
+        CurrentXPosition = transform.position.x;
+        Speed = (CurrentXPosition - LastXPosition) * 10 ;
+      
+   
+        transform.rotation = new Quaternion(0f, 0f, 0f, 1);
+        animator.SetFloat("Speed", Speed);
+    }
+}
